af2ee135b956822c67789fd569234bfb1bea774d
Delakzz <wizpizz.jaoberonio@gmail.com>
2026-08-31 22:46:08 +0800

    track other methods

--- model/track.go
+++ model/track.go
@@ -20,3 +20,32 @@ func (t *Track) SetTitle(title string) {
 func (t *Track) SetArtist(artist string) {
 	t.Artist = artist
 }
+
+func (t *Track) Equals(other Track) bool {
+	return t.Title == other.Title && t.Artist == other.Artist
+}
+
+func (t *Track) String() string {
+	return t.Title + " by " + t.Artist
+}
+
+func (t *Track) IsEmpty() bool {
+	return t.Title == "" && t.Artist == ""
+}
+
+func (t *Track) Copy() Track {
+	return Track{
+		Title:  t.Title,
+		Artist: t.Artist,
+	}
+}
+
+func (t *Track) UpdateFrom(other Track) {
+	t.Title = other.Title
+	t.Artist = other.Artist
+}
+
+func (t *Track) Clear() {
+	t.Title = ""
+	t.Artist = ""
+}
