2761532997bdd506912952266135a4e2c191449a
Delakzz <wizpizz.jaoberonio@gmail.com>
2026-09-01 23:50:51 +0800

    remove methods in arraylist

--- arraylist.go
+++ arraylist.go
@@ -22,6 +22,11 @@ func (a *ArrayList) incrementSize() {
 	a.size++
 }
 
+// decrementSize lessen the size of ArrayList by 1.
+func (a *ArrayList) decrementSize() {
+	a.size--
+}
+
 // AddFirst adds a new track to the front, making it the topmost of first element.
 func (a *ArrayList) AddFirst(track Track) {
 	a.arrayList = append([]Track{track}, a.arrayList...)
@@ -38,4 +43,25 @@ func (a *ArrayList) AddLast(track Track) {
 func (a *ArrayList) Add(track Track, idx int) {
 	a.arrayList = append(a.arrayList[:idx], append([]Track{track}, a.arrayList[idx:]...)...)
 	a.incrementSize()
+}
+
+// RemoveAtIndex removes the element at a specified index.
+func (a *ArrayList) RemoveAtIndex(idx int) Track {
+	target := a.arrayList[idx]
+	a.arrayList = append(a.arrayList[:idx], a.arrayList[idx+1:]...)
+	a.decrementSize()
+	return target
+}
+
+// Remove removes the first occurence of track from the ArrayList. Returns the removed track, or none if not found.
+func (a *ArrayList) Remove(track Track) Track {
+	var target Track
+	for i, t := range a.arrayList {
+		if t == track {
+			target = t
+			a.RemoveAtIndex(i)
+			break
+		}
+	}
+	return target
 }
