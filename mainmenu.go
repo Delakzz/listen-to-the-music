a57d8a9aaf7158b338d15940ce598a788bc15f19
Delakzz <wizpizz.jaoberonio@gmail.com>
2026-09-03 22:03:12 +0800

    feat

--- mainmenu.go
+++ mainmenu.go
@@ -1,20 +1,44 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 var MainMenu = Menu{
 	Title: "Main",
 	Items: []MenuItem{
+		{Label: "Add Track to Playlist", Action: handleAddTrack},
+		{Label: "View Playlist", Action: handleViewPlaylist},
+		{Label: "Play Playlist", Action: handlePlayPlaylist},
 		{Label: "Exit", Action: handleExit},
 	},
 }
 
-// func handleAddTrack()
+func handleAddTrack() bool {
+	title := ReadString("Title of the track")
+	artist := ReadString("Track Artist")
+	track := Track{Title: title, Artist: artist}
+	MainQueue.Enqueue(track)
+	fmt.Println("Track has been added to the queue!")
+	return true
+}
 
-// func handleViewPlaylist()
+func handleViewPlaylist() bool {
+	fmt.Println()
+	showHeader("My Playlist")
+	fmt.Println(MainQueue.String())
+	showHeader("End")
+	scanner.Scan()
+	return true
+}
 
-// func handlePlayPlaylist()
+func handlePlayPlaylist() bool {
+	PlayPlaylistMenu.Run()
+	return true
+}
 
-func handleExit() {
+func handleExit() bool {
 	os.Exit(0)
+	return true
 }
